131e42ff91c906f44db36cb474759017aa024f13
Dornam Wu <j.r.bbson@gmail.com>
2017-05-24 00:44:30 +0800

    demo

---
+++ scripts/mp.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Hello! ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Hello, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
